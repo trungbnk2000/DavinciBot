bffce3b481aa2e1b7ffc6e89d2f8b1805eba2c60
Tran Trung <trungt@tandan.com.vn>
2023-01-10 09:36:57 +0700

    First commit

---
+++ server.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import { Configuration, OpenAIApi } from 'openai';  
+
+dotenv.config()
+
+const configuration = new Configuration({
+    apiKey: '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'
+})
+
+const openai = new OpenAIApi(configuration);
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.post('/', async (req, res) => {
+    try{
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: req.body.input,
+            temperature: 0.7,
+            max_tokens: 256,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0,
+        });
+
+        res.status(200).send({
+            bot: response.data.choices[0].text
+        })
+    }
+    catch (error){
+        res.status(500).send({error})
+    }
+})
+
+app.listen(3000, () => console.log("Server is running on port 3000..."));
